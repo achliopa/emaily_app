546602e3326c1f09531f3680c6630ff942f26121
Sakis Chliopanos <athchliopanos@gmail.com>
2017-11-22 14:05:31 +0000

    save

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const ip = process.env.IP || undefined;
 
-// client ID: 465024689602-5f782cs2sn10o9130f5le4f5i0rlbbun.apps.googleusercontent.com
-// secret: 8I9lpUTyfgPU6i1Y4ywbI_Yz
+
 //passport.use(new GoogleStrategy());
 
 app.listen(port, ip, () => {
