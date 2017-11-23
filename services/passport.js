d36a264cfcd3c9f69df97e7b1e98419779315cf2
Sakis Chliopanos <athchliopanos@gmail.com>
2017-11-23 14:07:42 +0000

    lecture 40

--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,9 @@
 const passport =  require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys  = require('../config/keys');
-
+const mongoose = require('mongoose');
+// our model class
+const User = mongoose.model('users'); 
 
 // we set a arrow callback to be called when the google rplies with some data
 passport.use(new GoogleStrategy(
@@ -10,8 +12,20 @@ passport.use(new GoogleStrategy(
         clientSecret: keys.googleClientSecret,
         callbackURL: 'https://emaily-app-achliopa.c9users.io/auth/google/callback'
     }, (accessToken, refreshToken, profile, done) => {
-        console.log('access token: ', accessToken);
-        console.log('refresh token: ', refreshToken);
-        console.log('profile: ', profile);
+        // console.log('access token: ', accessToken);
+        // console.log('refresh token: ', refreshToken);
+        // console.log('profile: ', profile);
+        User.findOne({googleId: profile.id})
+        .then((existingUser) => {
+            if (existingUser) {
+                // we already have a record with this id
+                done(null, existingUser);
+            } else {
+                new User({googleId: profile.id}).save()
+                .then((user) => {
+                    done(null, user);
+                });                
+            }
+        });
     })
 );
